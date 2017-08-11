692ad9e9348ebba3af115c9cb023ab58a7efd8a8
arunnatva <arun.natva@gmail.com>
2017-08-11 18:21:00 -0400

    Create PresentValueApp.scala

---
+++ PresentValueApp.scala
@@ -0,0 +1,10 @@
+// This piece of code is run on the spark-shell, not with the spark-submit.
+import org.apache.spark.sql.functions._
+import org.apache.poi.ss.formula.functions.FinanceLib
+val mydf1 = sqlContext.read.json("/user/aknatva/sample.dat")
+mydf1.show()
+//Below statement creates a UDF that accepts three variables and calculates present value (its a financial function)
+val getPvUdf = udf((wacc:Double,lor:Double,revenue:Double) => FinanceLib.pv(wacc,lor,revenue,0,false))
+// A new dataframe is created by adding a derived column to existing dataframe
+val mydf2 = mydf1.withColumn("pv_val",getPvUdf($"wacc",$"lor",$"revenue")
+mydf2.show()
